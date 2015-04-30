35722f27b2f25669038875e73cdc1bd42af4dba6
Mariano <mariano2455@hotmail.com>
2015-04-30 13:25:02 +0200

    Instalacion de beautiful soup 4

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='Spottle',
       author='Mariano Triguero Marquez',
       author_email='mariano2455@hotmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['bottle','markdown','bs4']
+      install_requires=['bottle','markdown','beautifulsoup4']
      )
